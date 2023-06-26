6ff07db91bc931d5324762760aa6a416ef8c3924
Vante <136744983+vante-xyz@users.noreply.github.com>
2023-06-27 01:14:48 +0200

    Update guildDelete.js

--- src/Events/Vante/guildDelete.js
+++ src/Events/Vante/guildDelete.js
@@ -1,7 +1,7 @@
 const { Events } = require("discord.js"), { green, red } = require("chalk");
 
 module.exports = async (client, guild) => {
-    client.logger.log(`( ${red(`GUILD LEFT`)} ) ${guild.name} (${guild.id}) added the bot.`);
+    client.logger.log(`( ${red(`GUILD LEFT`)} ) ${guild.name} (${guild.id}) kicked the bot.`);
     await client.deleteGuild(guild);
 };
   
